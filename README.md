ec3b2027c5d5337b8983559b7fdcbe35908b2f26
Korolev Pavel <korolev.vrn@gmail.com>
2019-11-02 18:52:58 +0300

    Добавлен модуль users.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pws-4-b4-12
 Анализ базы данных атлетов Олимпийских Игр в Сочи
+
+## Модуль users.py
+Запуск: `python users.py "path sqlite base"`
--- users.py
+++ users.py
@@ -1,8 +1,9 @@
-import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
+import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
+import sys
 
-DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+DB_PATH = "sqlite:///" + sys.argv[1]
 Base = declarative_base()
 
 class User(Base):
@@ -10,9 +11,49 @@ class User(Base):
     __tablename__ = "user"
 
     id = sa.Column(sa.INTEGER, primary_key=True) 
-    name = sa.Column(sa.TEXT)
+    first_name = sa.Column(sa.TEXT)
     last_name = sa.Column(sa.TEXT)
     gender = sa.Column(sa.TEXT)
     email = sa.Column(sa.TEXT)
     birthdate = sa.Column(sa.TEXT)
     height = sa.Column(sa.FLOAT)
+
+def connect_db():
+
+    engine = sa.create_engine(DB_PATH)
+    Sessions = sessionmaker(engine)
+    session = Sessions()
+    return session
+
+def request_data():
+
+    print("Добрый день! Сейчас произойдет регистрация пользователя в базе данных")
+    first_name = input("Введите имя: ")
+    last_name = input("Введите фамилию: ")
+    gender = input("Укажите свой пол (Male - мужской, Female - женский): ")
+    email = input("Введите адрес электронной почты: ")
+    birthdate = input("Укажите дату рождения: ")
+    height = input("Укажите свой рост: ")
+
+    user = User(
+        first_name = first_name,
+        last_name = last_name,
+        gender = gender,
+        email = email,
+        birthdate = birthdate,
+        height = height
+    )
+    
+    return user
+
+def main():
+
+    session = connect_db()
+    new_user = request_data()
+    session.add(new_user)
+    session.commit()
+
+    print("Пользователь зарегистрирован!")
+
+if __name__ == "__main__":
+    main()
