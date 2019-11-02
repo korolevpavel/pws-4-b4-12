d17496d85888ab111a66c98de02613915462d17e
Korolev Pavel <korolev.vrn@gmail.com>
2019-11-02 17:57:51 +0300

    Init

---
+++ users.py
@@ -0,0 +1,18 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+Base = declarative_base()
+
+class User(Base):
+
+    __tablename__ = "user"
+
+    id = sa.Column(sa.INTEGER, primary_key=True) 
+    name = sa.Column(sa.TEXT)
+    last_name = sa.Column(sa.TEXT)
+    gender = sa.Column(sa.TEXT)
+    email = sa.Column(sa.TEXT)
+    birthdate = sa.Column(sa.TEXT)
+    height = sa.Column(sa.FLOAT)
