8ee662595af561604806979533e27d31eb2a3667
Korolev Pavel <korolev.vrn@gmail.com>
2019-11-04 12:30:41 +0300

    Добавлен поиск по росту

--- find_athlete.py
+++ find_athlete.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 import sys
-import datetime
+from datetime import datetime
 
 DB_PATH = "sqlite:///" + sys.argv[1]
 Base = declarative_base()
@@ -49,16 +49,78 @@ def request_data():
     print("Поиск атлетов")
     user_id = input("Введите идентификатор пользователя: ")
     
-    return user
+    return user_id
+
+def find_bd(user, session):
+    
+    all_athletes = session.query(Athelete).all()
+    id_athletes = {}
+
+    for athlete in all_athletes:
+        birthday = str_to_date(athlete.birthdate)
+        id_athletes[athlete.id] = birthday
+
+    user_birthday = str_to_date(user.birthdate)
+    nearest = ""
+    athlete_id = ""
+    athlete_birthday = ""
+
+    for id_athlete, birthday_athlete in id_athletes.items():
+        nearest_abs = abs(user_birthday - birthday_athlete)
+        if not nearest or nearest_abs < nearest:
+            nearest =nearest_abs
+            athlete_id = id_athlete
+            athlete_birthday = birthday_athlete
+    return athlete_id, athlete_birthday
+
+def find_height(user, session):
+     
+    all_athletes = session.query(Athelete).all()
+    id_athletes = {}
+
+    for athlete in all_athletes:
+        if athlete.height:
+            id_athletes[athlete.id] = athlete.height
+
+    user_heigth = user.height
+    nearest = ""
+    athlete_id = ""
+    athlete_heigth = ""
+
+    for id_athlete, heigth_athlete in id_athletes.items():
+        
+        if heigth_athlete is None:
+            continue
+        
+        nearest_abs = abs(user_heigth - heigth_athlete)
+        if not nearest or nearest_abs < nearest:
+            nearest =nearest_abs
+            athlete_id = id_athlete
+            athlete_heigth = heigth_athlete
+
+    return athlete_id, athlete_heigth
+
+def str_to_date(date_str):
+
+    date_test = date_str.replace("-", " ")
+    datetime_object = datetime.strptime(date_test, "%Y %m %d")
+   
+    return datetime_object.date()
 
 def main():
 
     session = connect_db()
-    new_user = request_data()
-    session.add(new_user)
-    session.commit()
+    
+    user_id = request_data()
+    user = session.query(User).filter(User.id == user_id).first()
 
-    print("Пользователь зарегистрирован!")
+    if not user:
+        print("Пользователь не найден в базе данных")
+    else:
+        athlete_id_birthday, athlete_birthday = find_bd(user, session)
+        print("Ближайший атлет по дате рождения: {} родился: {}".format(athlete_id_birthday, athlete_birthday))
+        athlete_id_height, athlete_heigth = find_height(user, session)
+        print("Ближайший атлет по весу: {} родился: {}".format(athlete_id_height, athlete_heigth))
 
 if __name__ == "__main__":
     main()
